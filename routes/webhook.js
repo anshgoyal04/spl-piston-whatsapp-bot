723da362752b9d05eaa3b8919e60c0874aef12e1
Ansh <anshhgoyal04gmail.com>
2026-06-03 05:16:05 -0700

    First commit

--- routes/webhook.js
+++ routes/webhook.js
@@ -2,25 +2,29 @@ const express = require("express");
 
 const router = express.Router();
 
-// Verify Webhook
+// Webhook Verification
 router.get("/", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
-console.log("🔥 WEBHOOK HIT");
-  console.log(JSON.stringify(req.body, null, 2));
 
-  res.sendStatus(200);
   if (
     mode === "subscribe" &&
     token === process.env.VERIFY_TOKEN
   ) {
-    console.log("Webhook Verified");
-
+    console.log("✅ Webhook Verified");
     return res.status(200).send(challenge);
   }
 
   return res.sendStatus(403);
+});
+
+// Incoming Messages
+router.post("/", (req, res) => {
+  console.log("🔥 WEBHOOK HIT");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  return res.sendStatus(200);
 });
 
 module.exports = router;
